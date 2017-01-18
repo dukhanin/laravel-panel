19a436941063dfa896d630219e9cbbc4fdad8e98
Anton Dukhanin <anton.dukhanin@gmail.com>
2017-01-18 18:29:25 +0500

    js- and css-assets refactored

--- public/assets/js/panel.list.js
+++ public/assets/js/panel.list.js
@@ -1,28 +1,41 @@
 panel.list = function (node) {
-    this.node                  = $(node);
-    this.rows                  = this.node.find('tbody:not(.panel-list-empty)>tr');
-    this.dataCells             = this.rows.find('.panel-list-data-cell');
-    this.checkboxes            = this.rows.find('tbody .panel-list-checkbox input');
-    this.checkboxesCheckAll    = this.node.find('thead .panel-list-checkbox input');
-    this.lastClickedRow        = null;
-    this.lastClickedRowChecked = null;
-    this.actionsButtons        = this.node.find('.panel-list-action');
-    this.groupActionsButtons   = this.node.find('.panel-list-group-action');
-    this.modelActionsButtons   = this.node.find('.panel-list-model-action a, .panel-list-model-action button');
-    this.categoriesSelect      = this.node.find('.panel-list-categories-select');
-    this.moveToSelect          = this.node.find('.panel-list-move-to-select');
+    this.node = $(node);
 };
 
 panel.list.prototype.init = function () {
+    this.initDomProperties();
+    this.initParams();
     this.initActions();
     this.initGroupActions();
     this.initModelActions();
+    this.initActionsOnHover();
     this.initCheckboxes();
     this.initCategories();
     this.initMoveTo();
     this.initDoubleClick();
+    this.initSortable();
 
     console.log('panel.list.init');
+};
+
+panel.list.prototype.initDomProperties = function () {
+    this.form                = this.node.find('.panel-list-form');
+    this.rows                = this.node.find('tbody:not(.panel-list-empty)>tr');
+    this.dataCells           = this.rows.find('.panel-list-data-cell');
+    this.checkboxes          = this.rows.find('.panel-list-checkbox input');
+    this.checkboxesCheckAll  = this.node.find('thead .panel-list-checkbox input');
+    this.actionsButtons      = this.node.find('.panel-list-action');
+    this.groupActionsButtons = this.node.find('.panel-list-group-action');
+    this.modelActionsButtons = this.node.find('.panel-list-model-action a, .panel-list-model-action button');
+    this.categoriesSelect    = this.node.find('.panel-list-categories-select');
+    this.moveToSelect        = this.node.find('.panel-list-move-to-select');
+};
+
+panel.list.prototype.initParams = function () {
+    this.url                        = this.form.attr('action');
+    this.lastClickedCheckbox        = null;
+    this.lastClickedCheckboxChecked = null;
+    this.sortableInProgress         = false;
 };
 
 panel.list.prototype.initActions = function () {
@@ -31,15 +44,14 @@ panel.list.prototype.initActions = function () {
 
 panel.list.prototype.initGroupActions = function () {
     this.groupActionsButtons.click(function (e) {
-        var button  = $(this),
-            confirm = button.data('confirm'),
-            url     = button.data('url'),
-            form    = button.closest('form');
-
-        var callback = function () {
-            form.attr('action', url);
-            form.submit();
-        };
+        var button   = $(this),
+            confirm  = button.data('confirm'),
+            url      = button.data('url'),
+            form     = button.closest('form'),
+            callback = function () {
+                form.attr('action', url);
+                form.submit();
+            };
 
         if (confirm) {
             panel.confirm(confirm, callback);
@@ -53,30 +65,50 @@ panel.list.prototype.initGroupActions = function () {
 
 panel.list.prototype.initModelActions = function () {
     this.modelActionsButtons.click(function (e) {
-        var button  = $(this),
-            confirm = button.data('confirm'),
-            url     = button.attr('href');
-
-        var callback = function () {
-            panel.go()
-        };
+        var button   = $(this),
+            confirm  = button.data('confirm'),
+            url      = button.attr('href'),
+            callback = function () {
+                panel.go(url);
+            };
 
         if (confirm) {
-            e.preventDefault();
-
-            panel.confirm(confirm, function () {
-                panel.go(url)
-            });
+            panel.confirm(confirm, callback);
+        } else {
+            callback.call(this);
         }
+
+        e.preventDefault();
     });
+};
+
+panel.list.prototype.initActionsOnHover = function () {
+    $()
+        .add(this.actionsButtons)
+        .add(this.groupActionsButtons)
+        .add(this.modelActionsButtons)
+        .filter('[data-icon-on-hover]')
+        .on('focusin focusout mouseenter mouseleave', function (e) {
+                var node        = $(this),
+                    icon        = node.find('i'),
+                    iconDefault = node.data('icon'),
+                    iconOnHover = node.data('icon-on-hover');
+
+                if (undefined === iconDefault) {
+                    node.data('icon', icon.attr('class'));
+                }
+
+                icon.removeClass().addClass(e.type === 'focusin' || e.type === 'mouseenter' ? iconOnHover : iconDefault);
+            });
 };
 
 panel.list.prototype.initCategories = function () {
     this.categoriesSelect.change(function (e) {
-        var select = $(this);
-        var url    = select.data('url');
+        var select = $(this),
+            url    = select.data('url');
 
         panel.go(url.replace('dummyCategory', select.val()));
+
         e.preventDefault();
     });
 };
@@ -86,14 +118,13 @@ panel.list.prototype.initMoveTo = function () {
         var select = $(this);
 
         if (select.val()) {
-            var url     = select.data('url'),
-                confirm = select.data('confirm'),
-                form    = select.closest('form');
-
-            var callback = function () {
-                form.attr('action', url.replace('dummyMoveTo', select.val()));
-                form.submit();
-            };
+            var url      = select.data('url'),
+                confirm  = select.data('confirm'),
+                form     = select.closest('form'),
+                callback = function () {
+                    form.attr('action', url.replace('dummyMoveTo', select.val()));
+                    form.submit();
+                };
 
             panel.confirm(confirm, callback);
         }
@@ -103,86 +134,71 @@ panel.list.prototype.initMoveTo = function () {
 };
 
 panel.list.prototype.initCheckboxes = function () {
-    if (this.checkboxes.size() == 0) {
+    if (this.checkboxes.length == 0) {
         return;
     }
 
-    this.checkboxes.on('click', $.proxy(function (e) {
-        var tr = $(e.target).closest('tr');
+    this.checkboxes.on('change', function (e) {
+        $(this).trigger.call(this, e);
+    });
 
-        this.toggleRow(tr, e.target.checked);
+    this.checkboxes.on('state-changed', $.proxy(function (e) {
+        var checkbox   = $(e.target),
+            row        = checkbox.closest('tr'),
+            rowChecked = checkbox.prop('checked');
+
+        row.toggleClass('bg-warning selected', rowChecked);
         this.updateCheckboxesCheckAll();
-
-        this.lastClickedRow        = tr;
-        this.lastClickedRowChecked = e.target.checked;
     }, this));
 
-    this.checkboxesCheckAll.on('click', $.proxy(function (e) {
-        this.rows.each($.proxy(function (index, tr) {
-            this.toggleRow(tr, e.target.checked);
-        }, this));
-
-        this.updateCheckboxesCheckAll();
+    this.checkboxesCheckAll.on('change', $.proxy(function (e) {
+        this.checkboxes.prop('checked', $(e.target).prop('checked')).trigger('state-changed');
     }, this));
 
     this.dataCells.on('mousedown', $.proxy(function (e) {
-        var row        = $(e.target).closest('tr');
-        var rowChecked = this.toggleRow.call(this, row);
+        var checkbox = $(e.target).closest('tr').find('.panel-list-checkbox input'),
+            checked  = !checkbox.prop('checked');
 
-        if (e.shiftKey && this.lastClickedRow) {
-            var lastClickedRowindex = this.rows.index(this.lastClickedRow);
-            var currentRowIndex     = this.rows.index(row);
+        checkbox.prop('checked', checked).trigger('state-changed');
 
-            this.rows.slice(lastClickedRowindex, currentRowIndex).each($.proxy(function (index, row) {
-                this.toggleRow.call(this, row, rowChecked);
-            }, this));
+        if (e.shiftKey && this.lastClickedCheckbox) {
+            var lastClickedCheckboxIndex = this.checkboxes.index(this.lastClickedCheckbox),
+                currentCheckboxIndex     = this.checkboxes.index(checkbox);
+
+            this.checkboxes.slice(lastClickedCheckboxIndex, currentCheckboxIndex).prop('checked', checked).trigger('state-changed');
         }
 
-        this.updateCheckboxesCheckAll();
-
-        this.lastClickedRow        = row;
-        this.lastClickedRowChecked = rowChecked;
+        this.lastClickedCheckbox        = checkbox;
+        this.lastClickedCheckboxChecked = checked;
     }, this));
 
     this.dataCells.on('mouseenter', $.proxy(function (e) {
+        if (this.sortableInProgress || this.lastClickedCheckbox === null) {
+            return;
+        }
+
         if (e.buttons == 1 || e.buttons == 3) {
-            this.toggleRow.call(this, e.target, this.lastClickedRowChecked);
-            this.updateCheckboxesCheckAll();
+            var checkbox = $(e.target).closest('tr').find('.panel-list-checkbox input');
+            checkbox.prop('checked', this.lastClickedCheckboxChecked).trigger('state-changed');
         }
     }, this));
 };
 
 panel.list.prototype.initDoubleClick = function () {
-    this.rows.on('dblclick', function () {
-        var row            = $(this);
-        var actionSelector = row.data('dblclick');
+    this.dataCells.on('dblclick', function (e) {
+        var row            = $(e.target).closest('tr'),
+            actionSelector = row.data('dblclick'),
+            url            = row.find(actionSelector).attr('href');
 
-        if (actionSelector) {
-            var url = row.find(actionSelector).attr('href');
-            url && panel.go(url);
+        if (actionSelector && url) {
+            panel.go(url);
         }
     });
-
 };
-
-panel.list.prototype.toggleRow = function (target, toggle) {
-    var tr    = $(target).closest('tr');
-    var input = tr.find('td.panel-list-checkbox input');
-
-    if ('undefined' === typeof toggle) {
-        toggle = !input.prop('checked');
-    }
-
-    tr.toggleClass('bg-warning', toggle);
-    input.prop('checked', toggle);
-
-    return toggle;
-};
-
 
 panel.list.prototype.updateCheckboxesCheckAll = function () {
-    var isAnyChecked   = this.checkboxes.filter(':checked').size() > 0;
-    var isAnyUnchecked = this.checkboxes.filter(':not(:checked)').size() > 0;
+    var checked = this.checkboxes.length > 0 && this.checkboxes.length == this.checkboxes.filter(':checked').length;
 
-    this.checkboxesCheckAll.prop('checked', isAnyChecked && !isAnyUnchecked);
+    this.checkboxesCheckAll.prop('checked', checked);
+    this.checkboxesCheckAll.parent().toggleClass('checked', checked);
 };
