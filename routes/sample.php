16eb861b37fdd72ab459eaad4b6f15312fbb5a46
Anton Dukhanin <anton.dukhanin@gmail.com>
2017-02-15 21:44:37 +0500

    routes updated

--- routes/sample.php
+++ routes/sample.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\Sample\SampleProductsController;
 use App\Http\Controllers\Sample\SampleSectionsController;
 
 Route::group([ 'prefix' => 'sample/products' ], function () {
-    SampleProductsController::routes([ 'as' => 'products' ]);
+    SampleProductsController::addRoutes([ 'as' => 'products' ]);
 });
 
 Route::group([ 'prefix' => 'sample/sections' ], function () {
-    SampleSectionsController::routes([ 'as' => 'sections' ]);
+    SampleSectionsController::addRoutes([ 'as' => 'sections' ]);
 });
 
 Route::any('sample', function () {
