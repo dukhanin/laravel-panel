1db8b364029b92314ca579cc5ea0b95495dcab5e
Anton Dukhanin <anton.dukhanin@gmail.com>
2017-03-14 12:17:33 +0500

    PanelModel trait and his friends PanelCollection and PanelBuilder classes

---
+++ src/Traits/PanelModel.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Dukhanin\Panel\Traits;
+
+use Dukhanin\Panel\Collections\PanelCollection;
+use Dukhanin\Panel\Query\PanelBuilder;
+
+trait PanelModel
+{
+
+    protected $nestedCollection;
+
+    public $nestedDepth;
+
+
+    public function getParentKeyName()
+    {
+        return 'parent_id';
+    }
+
+
+    public function newEloquentBuilder($query)
+    {
+        $builder = new PanelBuilder($query);
+
+        $builder->keyName       = $this->getKeyName();
+        $builder->parentKeyName = $this->getParentKeyName();
+
+        return $builder;
+
+    }
+
+
+    public function initNestedCollection()
+    {
+        $this->nestedCollection = $this->newCollection();
+    }
+
+
+    public function nestedCollection()
+    {
+        if (is_null($this->nestedCollection)) {
+            $this->initNestedCollection();
+        }
+
+        return $this->nestedCollection;
+    }
+
+
+    public function nestedDepth($value = null)
+    {
+        if (func_num_args() === 0) {
+            return $this->nestedDepth;
+        }
+
+        $this->nestedDepth = is_null($value) ? null : intval($value);
+    }
+
+
+    public function newCollection(array $models = [])
+    {
+        $collection = new PanelCollection($models);
+
+        $collection->keyName       = $this->getKeyName();
+        $collection->parentKeyName = $this->getParentKeyName();
+
+        return $collection;
+    }
+}
