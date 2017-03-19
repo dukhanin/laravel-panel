30859fa22c0fb2e0219af47d2b2ec8d322d6317c
Anton Dukhanin <anton.dukhanin@gmail.com>
2017-03-19 17:43:42 +0500

    date field

--- resources/views/panel-inspinia/init.blade.php
+++ resources/views/panel-inspinia/init.blade.php
@@ -15,7 +15,7 @@
 <script src="{{ URL::asset('assets/panel-inspinia/js/jquery.multisortable.js') }}"></script>
 <script src="{{ URL::asset('assets/panel-inspinia/js/cropper.min.js') }}"></script>
 <script src="{{ URL::asset('assets/panel-inspinia/js/bootstrap-datepicker.js') }}"></script>
-<script src="{{ URL::asset('assets/panel-inspinia/js/bootstrap-datepicker.ru.js') }}"></script>
+@if(app()->getLocale()  != 'en')<script src="{{ URL::asset('assets/panel-inspinia/js/bootstrap-datepicker.' . app()->getLocale() . '.js') }}"></script>@endif
 <script src="{{ URL::asset('assets/tinymce/tinymce.jquery.min.js') }}"></script>
 <script src="{{ URL::asset('assets/tinymce/jquery.tinymce.min.js') }}"></script>
 
@@ -35,7 +35,7 @@
             forceParse: true,
             calendarWeeks: true,
             autoclose: true,
-            language: '{{ config('app.locale') }}'
+            language: '{{ app()->getLocale() }}'
         });
 
         panel.uploadUrl = '{{ route('panel.upload.form') }}';
