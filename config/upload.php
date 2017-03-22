98208856c9ed974d3156b14de2197ad5cc6b3528
Anton Dukhanin <anton.dukhanin@gmail.com>
2017-03-22 18:46:32 +0500

    quality option in config

--- config/upload.php
+++ config/upload.php
@@ -3,5 +3,8 @@
 return [
     'path' => public_path('upload'),
     'url'  => '/upload',
-    'unauthorized_upload' => true
+    'unauthorized_upload' => true,
+    'images' => [
+        'quality' => 90
+    ]
 ];
